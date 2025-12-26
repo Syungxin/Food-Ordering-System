a1d408a986c6934ebb6ab22c5ae72125f5e2176d
Syungxin <Syungxin21@gmail.com>
2025-12-26 21:11:15 +0800

    Updated latest auto deploy so ec2 user can run git pull

--- deploy.php
+++ deploy.php
@@ -1,9 +1,9 @@
 <?php
-// Execute deployment
-$output = [];
-exec('cd /var/www/html && git pull origin main 2>&1', $output);
-
-// Return output
 header('Content-Type: text/plain');
+
+$cmd = 'sudo -u ec2-user /usr/bin/git -C /var/www/html pull origin main 2>&1';
+exec($cmd, $output);
+
 echo implode("\n", $output);
+
 ?>
