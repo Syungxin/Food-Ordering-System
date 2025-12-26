e26461e00e290c789db3daa371c7a278bd2172e5
Syungxin <Syungxin21@gmail.com>
2025-12-26 21:01:37 +0800

    Auto Deploy

--- deploy.php
+++ deploy.php
@@ -1,6 +1,9 @@
 <?php
-    exec('cd /var/www/html && git pull 2>&1', $output);
-    echo "<pre>";
-    print_r($output);
-    echo "</pre>";
+// Execute deployment
+$output = [];
+exec('cd /var/www/html && git pull origin main 2>&1', $output);
+
+// Return output
+header('Content-Type: text/plain');
+echo implode("\n", $output);
 ?>
