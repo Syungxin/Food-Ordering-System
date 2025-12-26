971363df22769a11572019e9a275591c0728b152
Syungxin <Syungxin21@gmail.com>
2025-12-26 21:13:43 +0800

    Removed db connection indicator

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -8,8 +8,6 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
-}else{
-    echo "✅ Connected to RDS successfully!";
 }
 ?>
 
